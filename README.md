7f03134146fa0031fc4e0900fb1f8e20af4683d7
huatao <huatao@huataodeMacBook-Pro.local>
2018-07-02 20:19:00 +0800

    修改

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # convremvw-loader
+
 Rem or vm conversion
+可以转换 rem 又可以转换 vm 的 loader ##安装
+
+```
+npm install convremvw-loader
+```
+
+默认转换 rem，如果是 rem 还需引入来进行根元素font-size的变化
+
+```
+function resize () {
+			document.documentElement.setAttribute('style', 'font-size:'+document.documentElement.clientWidth / 7.5 + 'px !important');
+}
+		document.addEventListener('DOMContentLoaded',function() {
+				resize()
+		window.onresize = function() {
+			resize()
+		}
+	})
+```
+##参数
+```
+module: {
+  rules: [{
+    test: /\.css$/,
+      use: [{
+        loader: 'convremvw-loader',
+        query: {
+         unit: 'rem'或者是'vw'
+        }
+      }, {
+        loader: "css-loader"
+      }]
+  }]
+}
+```
--- index.js
+++ index.js
@@ -17,20 +17,17 @@ module.exports = function (source) {
         }
     }
     var query = Object.assign({},{
-        viewportUnit: 'rem',
+        unit: 'rem',
     },getQueryString(this.query));
     var matchPXExp = /([0-9.]+px)([;,| |}|'|"\)\r|\n])/g;
 
     return source.replace(matchPXExp, function (match, m1, m2) {
         var pixels = parseFloat(m1.slice(0, m1.length - 2));
-        if (pixels <= query.minPixelValue) {
-            return match;
-        }
-     function createPxReplace(pixels,viewportUnit) {
-            var test=(pixels * (1/100)).toFixed(2) + viewportUnit;
+     function createPxReplace(pixels,unit) {
+            var test=(pixels * (1/100)).toFixed(2) + unit;
             return test
        }
-        return createPxReplace(pixels,query.viewportUnit) + m2;
+        return createPxReplace(pixels,query.unit) + m2;
     });
 
 };
